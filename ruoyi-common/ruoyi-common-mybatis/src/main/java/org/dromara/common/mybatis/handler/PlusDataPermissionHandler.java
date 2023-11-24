3fed765d6c28d7b86c46f5cdfb926d2bfb3f71d2
yhan219 <yhan219@sina.com>
2023-11-24 18:22:38 +0800

    同步上游ruoyi vue plus 5.1.1

--- ruoyi-common/ruoyi-common-mybatis/src/main/java/org/dromara/common/mybatis/handler/PlusDataPermissionHandler.java
+++ ruoyi-common/ruoyi-common-mybatis/src/main/java/org/dromara/common/mybatis/handler/PlusDataPermissionHandler.java
@@ -1,15 +1,10 @@
 package org.dromara.common.mybatis.handler;
 
-import cn.hutool.core.annotation.AnnotationUtil;
 import cn.hutool.core.collection.CollUtil;
-import cn.hutool.core.util.ClassUtil;
+import cn.hutool.core.util.ArrayUtil;
 import cn.hutool.core.util.ObjectUtil;
+import com.mybatisflex.core.query.QueryWrapper;
 import lombok.extern.slf4j.Slf4j;
-import net.sf.jsqlparser.JSQLParserException;
-import net.sf.jsqlparser.expression.Expression;
-import net.sf.jsqlparser.expression.Parenthesis;
-import net.sf.jsqlparser.expression.operators.conditional.AndExpression;
-import net.sf.jsqlparser.parser.CCJSqlParserUtil;
 import org.dromara.common.core.domain.dto.RoleDTO;
 import org.dromara.common.core.domain.model.LoginUser;
 import org.dromara.common.core.exception.ServiceException;
@@ -21,12 +16,6 @@ import org.dromara.common.mybatis.annotation.DataPermission;
 import org.dromara.common.mybatis.enums.DataScopeType;
 import org.dromara.common.mybatis.helper.DataPermissionHelper;
 import org.dromara.common.satoken.utils.LoginHelper;
-import lombok.extern.slf4j.Slf4j;
-import net.sf.jsqlparser.JSQLParserException;
-import net.sf.jsqlparser.expression.Expression;
-import net.sf.jsqlparser.expression.Parenthesis;
-import net.sf.jsqlparser.expression.operators.conditional.AndExpression;
-import net.sf.jsqlparser.parser.CCJSqlParserUtil;
 import org.springframework.context.expression.BeanFactoryResolver;
 import org.springframework.expression.BeanResolver;
 import org.springframework.expression.ExpressionParser;
@@ -34,25 +23,22 @@ import org.springframework.expression.ParserContext;
 import org.springframework.expression.common.TemplateParserContext;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.expression.spel.support.StandardEvaluationContext;
+import org.springframework.stereotype.Component;
 
-import java.lang.reflect.Method;
-import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.function.Function;
 
 /**
- * 数据权限过滤
+ * 数据权限处理器
  *
- * @author Lion Li
- * @version 3.5.0
+ * @author yhan219
+ * @see <a href="https://gitee.com/dromara/RuoYi-Vue-Plus/blob/5.X/ruoyi-common/ruoyi-common-mybatis/src/main/java/org/dromara/common/mybatis/handler/PlusDataPermissionHandler.java">参考</a>
  */
 @Slf4j
+@Component
 public class PlusDataPermissionHandler {
-
-    /**
-     * 方法或类(名称) 与 注解的映射关系缓存
-     */
-    private final Map<String, DataPermission> dataPermissionCacheMap = new ConcurrentHashMap<>();
 
     /**
      * spel 解析器
@@ -65,8 +51,14 @@ public class PlusDataPermissionHandler {
     private final BeanResolver beanResolver = new BeanFactoryResolver(SpringUtils.getBeanFactory());
 
 
-    public Expression getSqlSegment(Expression where, String mappedStatementId, boolean isSelect) {
-        DataColumn[] dataColumns = findAnnotation(mappedStatementId);
+    public void handlerDataPermission(DataPermission dataPermission, QueryWrapper queryWrapper, boolean isSelect) {
+        if (dataPermission == null) {
+            return;
+        }
+        DataColumn[] dataColumns = dataPermission.getValue();
+        if (ArrayUtil.isEmpty(dataColumns)) {
+            return;
+        }
         LoginUser currentUser = DataPermissionHelper.getVariable("user");
         if (ObjectUtil.isNull(currentUser)) {
             currentUser = LoginHelper.getLoginUser();
@@ -74,24 +66,18 @@ public class PlusDataPermissionHandler {
         }
         // 如果是超级管理员或租户管理员，则不过滤数据
         if (LoginHelper.isSuperAdmin() || LoginHelper.isTenantAdmin()) {
-            return where;
+            return;
         }
         String dataFilterSql = buildDataFilter(dataColumns, isSelect);
         if (StringUtils.isBlank(dataFilterSql)) {
-            return where;
+            return;
         }
-        try {
-            Expression expression = CCJSqlParserUtil.parseExpression(dataFilterSql);
-            // 数据权限使用单独的括号 防止与其他条件冲突
-            Parenthesis parenthesis = new Parenthesis(expression);
-            if (ObjectUtil.isNotNull(where)) {
-                return new AndExpression(where, parenthesis);
-            } else {
-                return parenthesis;
-            }
-        } catch (JSQLParserException e) {
-            throw new ServiceException("数据权限解析异常 => " + e.getMessage());
-        }
+        queryWrapper.and(dataFilterSql);
+    }
+
+
+    public String getSQL(DataPermission dataPermission, boolean isSelect) {
+        return buildDataFilter(dataPermission.getValue(), isSelect);
     }
 
     /**
@@ -118,18 +104,18 @@ public class PlusDataPermissionHandler {
             }
             boolean isSuccess = false;
             for (DataColumn dataColumn : dataColumns) {
-                if (dataColumn.key().length != dataColumn.value().length) {
-                    throw new ServiceException("角色数据范围异常 => key与value长度不匹配");
+                if (dataColumn.getKey().length != dataColumn.getValue().length) {
+                    throw new ServiceException("角色数据范围异常 => getKey与getValue长度不匹配");
                 }
-                // 不包含 key 变量 则不处理
+                // 不包含 getKey 变量 则不处理
                 if (!StringUtils.containsAny(type.getSqlTemplate(),
-                    Arrays.stream(dataColumn.key()).map(key -> "#" + key).toArray(String[]::new)
+                    Arrays.stream(dataColumn.getKey()).map(getKey -> "#" + getKey).toArray(String[]::new)
                 )) {
                     continue;
                 }
-                // 设置注解变量 key 为表达式变量 value 为变量值
-                for (int i = 0; i < dataColumn.key().length; i++) {
-                    context.setVariable(dataColumn.key()[i], dataColumn.value()[i]);
+                // 设置注解变量 getKey 为表达式变量 getValue 为变量值
+                for (int i = 0; i < dataColumn.getKey().length; i++) {
+                    context.setVariable(dataColumn.getKey()[i], dataColumn.getValue()[i]);
                 }
 
                 // 解析sql模板并填充
@@ -150,38 +136,5 @@ public class PlusDataPermissionHandler {
         return "";
     }
 
-    public DataColumn[] findAnnotation(String mappedStatementId) {
-        StringBuilder sb = new StringBuilder(mappedStatementId);
-        int index = sb.lastIndexOf(".");
-        String clazzName = sb.substring(0, index);
-        String methodName = sb.substring(index + 1, sb.length());
-        Class<?> clazz = ClassUtil.loadClass(clazzName);
-        List<Method> methods = Arrays.stream(ClassUtil.getDeclaredMethods(clazz))
-            .filter(method -> method.getName().equals(methodName)).toList();
-        DataPermission dataPermission;
-        // 获取方法注解
-        for (Method method : methods) {
-            dataPermission = dataPermissionCacheMap.get(mappedStatementId);
-            if (ObjectUtil.isNotNull(dataPermission)) {
-                return dataPermission.value();
-            }
-            if (AnnotationUtil.hasAnnotation(method, DataPermission.class)) {
-                dataPermission = AnnotationUtil.getAnnotation(method, DataPermission.class);
-                dataPermissionCacheMap.put(mappedStatementId, dataPermission);
-                return dataPermission.value();
-            }
-        }
-        dataPermission = dataPermissionCacheMap.get(clazz.getName());
-        if (ObjectUtil.isNotNull(dataPermission)) {
-            return dataPermission.value();
-        }
-        // 获取类注解
-        if (AnnotationUtil.hasAnnotation(clazz, DataPermission.class)) {
-            dataPermission = AnnotationUtil.getAnnotation(clazz, DataPermission.class);
-            dataPermissionCacheMap.put(clazz.getName(), dataPermission);
-            return dataPermission.value();
-        }
-        return null;
-    }
 
 }
